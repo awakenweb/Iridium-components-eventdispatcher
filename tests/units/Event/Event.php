b4e72e39659e6e27853ec253e08c68893d90fb30
Mathieu Savelli <mathieu.savelli@awakenweb.fr>
2014-01-12 14:28:29 +0100

    updated license text

--- tests/units/Event/Event.php
+++ tests/units/Event/Event.php
@@ -1,11 +1,5 @@
 <?php
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 namespace Iridium\Components\EventDispatcher\tests\units\Event;
 
 require_once __DIR__ . '/../../../vendor/autoload.php';
